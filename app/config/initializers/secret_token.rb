d87bdd0bc195bc74b08da379fec1a14829459626
Alan Graham <alan@productlab.com>
2013-04-25 15:30:21 -0400

    added demo app, added compass-rails dependency

---
+++ app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TodoList::Application.config.secret_token = '1c379f02de937578c658abee0a6f7a8878c83dfd171ba5d87364f321d7a1c77f57cb31a9290ac72c97acc2de3c5c342fb70d85008bb3a7f3778a8f31930eb01d'
