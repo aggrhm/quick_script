68fa5d9e58312c7368ea94463167c6038e14db54
Brian Taylor <el.wubo@gmail.com>
2016-07-11 21:46:44 -0400

    def_action support

--- lib/quick_script/model.rb
+++ lib/quick_script/model.rb
@@ -6,6 +6,12 @@ module QuickScript
       base.send :extend, ClassMethods
     end
 
+    # creates the instance bound to self of the anonymous "actions"
+    # class that resulted from the calls to def_action
+    def actions
+      @actions ||= self.class.actions_class.new(self)
+    end
+
     module ClassMethods
       def attr_alias(new_attr, old_attr)
         alias_method(new_attr, old_attr)
@@ -29,6 +35,35 @@ module QuickScript
           end
         end
       end
+
+      # class created once per mixin to contain all of the methods
+      # created using def_action
+      def actions_class
+        @actions_class ||= Class.new do
+          attr_reader :target
+
+          define_method(:initialize) do |target|
+            @target = target
+          end
+        end
+      end
+
+      # create an action inside the actions_class that wraps the body
+      # and returns the standard dictionary reply required for an
+      # action
+      def def_action(method_name, &block)
+        actions_class.class_eval do
+          define_method(method_name) do |*args|
+            begin
+              result = self.target.instance_exec(*args, &block)
+              {success: true, data: result}
+            rescue => ex
+              {success: false, reason: ex}
+            end
+          end
+        end
+      end
+
 
     end
 
